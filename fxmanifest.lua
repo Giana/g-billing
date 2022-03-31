a9cab8934e90ff6502a407d66010438506957057
Giana <giana@giana.dev>
2022-03-31 01:05:26 -0500

    Refactor

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,12 +8,12 @@ description 'billing'
 shared_script 'config.lua'
 
 client_scripts {
-	'client.lua',
+	'client/main.lua',
 }
 
 server_scripts {
 	'@oxmysql/lib/MySQL.lua',
-	'server.lua',
+	'server/main.lua',
 }
 
 dependencies {
